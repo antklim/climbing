55534bdb82e1aaf764e494120919dc54a11bbf79
antklim <antklim@gmail.com>
2017-03-15 23:22:50 +1100

    climber model updated

--- model.js
+++ model.js
@@ -6,6 +6,7 @@
   const World = Matter.World;
   const Body = Matter.Body;
   const Bodies = Matter.Bodies;
+  const Constraint = Matter.Constraint;
   const MouseConstraint = Matter.MouseConstraint;
   const Events = Matter.Events;
 
@@ -22,10 +23,25 @@
       showDebug: true,
       showCollisions: true,
       showPositions: true,
-      showConvexHulls: true
+      showConvexHulls: true,
+			wireframes: false,
+			background: '#111'
     }
   });
 
+  // categories
+  const defaultCategory = 0x0001;
+  const redCategory = 0x0002;
+  const greenCategory = 0x0004;
+  const blueCategory = 0x0008;
+
+  // colors
+  const redColor = '#C44D58';
+  const blueColor = '#4ECDC4';
+
+
+  // scene parts ===============================================================
+  // WALL
   const wallParts = [
     Bodies.rectangle(300, 100, 7, 7), Bodies.rectangle(400, 100, 7, 7), Bodies.rectangle(500, 100, 7, 7),
     Bodies.rectangle(300, 200, 7, 7), Bodies.rectangle(400, 200, 7, 7), Bodies.rectangle(500, 200, 7, 7),
@@ -33,42 +49,196 @@
     Bodies.rectangle(300, 400, 7, 7), Bodies.rectangle(400, 400, 7, 7), Bodies.rectangle(500, 400, 7, 7),
     Bodies.rectangle(300, 500, 7, 7), Bodies.rectangle(400, 500, 7, 7), Bodies.rectangle(500, 500, 7, 7),
   ]
-
   const wall = Body.create({
     parts: wallParts,
     isStatic: true
   });
+  World.add(world, wall);
 
-  const climberParts = [
-    // hands
-    Bodies.circle(150, 475, 5),
-    Bodies.circle(210, 475, 5),
 
-    // centre of gravity
-    Bodies.circle(180, 525, 9),
-
-    // feet
-    Bodies.circle(150, 575, 5),
-    Bodies.circle(210, 575, 5),
-  ];
-
-  const climber = Body.create({
-    parts: climberParts
-  });
-
+  // GROUND
   const ground = Bodies.rectangle(400, 610, 810, 60, {
-    isStatic: true
+    collisionFilter: {
+      category: defaultCategory
+    },
+    isStatic: true,
+		render: {
+      fillStyle: 'transparent',
+      lineWidth: 1
+    }
+  });
+  World.add(world, ground);
+
+
+  // CLIMBER
+	const torso = Bodies.circle(600, 400, 10, {
+    setMass: 10,
+    render: {
+      strokeStyle: blueColor,
+      fillStyle: 'transparent',
+      lineWidth: 1,
+    },
+    collisionFilter: {
+      category: blueCategory
+    }
+	});
+
+	const hip = Bodies.circle(600, 500, 30, {
+    setMass: 10,
+    render: {
+      strokeStyle: blueColor,
+      fillStyle: 'transparent',
+      lineWidth: 1,
+    },
+    collisionFilter: {
+      category: blueCategory
+    }
   });
 
-  const offset = 5;
-  World.add(world, [
-      Bodies.rectangle(400, -offset, 800.5 + 2 * offset, 50.5, { isStatic: true }),
-      Bodies.rectangle(400, 600 + offset, 800.5 + 2 * offset, 50.5, { isStatic: true }),
-      Bodies.rectangle(800 + offset, 300, 50.5, 600.5 + 2 * offset, { isStatic: true }),
-      Bodies.rectangle(-offset, 300, 50.5, 600.5 + 2 * offset, { isStatic: true })
+	const shoulderL = Bodies.circle(580, 400, 10, {
+    setMass: 1,
+    render: {
+      strokeStyle: blueColor,
+      fillStyle: 'transparent',
+      lineWidth: 1,
+    },
+    collisionFilter: {
+      category: blueCategory
+    }
+  });
+
+	const shoulderR = Bodies.circle(620, 400, 10, {
+    setMass: 1,
+    render: {
+      strokeStyle: blueColor,
+      fillStyle: 'transparent',
+      lineWidth: 1,
+    },
+    collisionFilter: {
+      category: blueCategory
+    }
+  });
+
+  const elbowL = Bodies.circle(560, 450, 10, {
+    setMass: 1,
+    restitution: 0.0001,
+    render: {
+      strokeStyle: blueColor,
+      fillStyle: 'transparent',
+      lineWidth: 1,
+    },
+    collisionFilter: {
+      category: blueCategory
+    }
+  });
+
+  const elbowR = Bodies.circle(640, 450, 10, {
+    setMass: 1,
+    restitution: 0.0001,
+    render: {
+      strokeStyle: blueColor,
+      fillStyle: 'transparent',
+      lineWidth: 1,
+    },
+    collisionFilter: {
+      category: blueCategory
+    }
+  });
+
+  const handL = Bodies.circle(540, 500, 10, {
+    setMass: 1,
+    render: {
+      strokeStyle: redColor,
+      fillStyle: 'transparent',
+      lineWidth: 1,
+    },
+    collisionFilter: {
+      category: redCategory
+    }
+  });
+
+  const handR = Bodies.circle(660, 500, 10, {
+    setMass: 1,
+    render: {
+      strokeStyle: redColor,
+      fillStyle: 'transparent',
+      lineWidth: 1,
+    },
+    collisionFilter: {
+      category: redCategory
+    }
+  });
+
+  const footL = Bodies.circle(580, 600, 10, {
+    setMass: 5,
+    render: {
+      strokeStyle: redColor,
+      fillStyle: 'transparent',
+      lineWidth: 1,
+    },
+    collisionFilter: {
+      category: redCategory
+    }
+  });
+
+  const footR = Bodies.circle(620, 600, 10, {
+    setMass: 5,
+    render: {
+      strokeStyle: redColor,
+      fillStyle: 'transparent',
+      lineWidth: 1,
+    },
+    collisionFilter: {
+      category: redCategory
+    }
+  });
+
+  World.add(world, [ torso, hip, shoulderL, shoulderR, elbowL, elbowR,
+    handL, handR, footL, footR
   ]);
 
-  World.add(world, [wall, climber]);
+  const armL = Constraint.create({ bodyA: shoulderL, bodyB: elbowL });
+  const armR = Constraint.create({ bodyA: shoulderR, bodyB: elbowR });
+  const forearmL = Constraint.create({ bodyB: elbowL, bodyA: handL });
+  const forearmR = Constraint.create({ bodyB: elbowR, bodyA: handR });
+  const spine = Constraint.create({ bodyA: torso, bodyB: hip });
+  const trapL = Constraint.create({ bodyA: torso, bodyB: shoulderL });
+  const trapR = Constraint.create({ bodyA: torso, bodyB: shoulderR });
+  const abL = Constraint.create({ bodyA: hip, bodyB: shoulderL });
+  const abR = Constraint.create({ bodyA: hip, bodyB: shoulderR });
+  const leg1L = Constraint.create({ bodyA: shoulderL, bodyB: footL, stiffness: 1 });
+  const leg2L = Constraint.create({ bodyA: hip, bodyB: footL, stiffness: 1 });
+  const leg1R = Constraint.create({ bodyA: shoulderR, bodyB: footR, stiffness: 1 });
+  const leg2R = Constraint.create({ bodyA: hip, bodyB: footR, stiffness: 1 });
+
+  World.add(world, [armL, armR, forearmL, forearmR, leg1L, leg1R, leg2L, leg2R,
+    spine, trapL, trapR, abL, abR
+  ]);
+
+  const calculateCog = (arrayOfBodies) => {
+    const r = arrayOfBodies.reduce((accumulator, body) => {
+      accumulator.x += body.position.x * body.mass;
+      accumulator.y += body.position.y * body.mass;
+      accumulator.m += body.mass;
+      return accumulator;
+    }, { x: 0, y: 0, m: 0 });
+
+    return {
+      x: r.x / r.m,
+      y: r.y / r.m
+    }
+  }
+
+  const cogCoordinates = calculateCog([torso, hip, shoulderL, shoulderR, footL, footR,
+    elbowL, elbowR, handL, handR
+  ]);
+  const cog = Bodies.circle(cogCoordinates.x, cogCoordinates.y, 5, {
+    isStatic: true,
+    collisionFilter: {
+      mask: redCategory | greenCategory
+    }
+  });
+  World.add(world, cog);
 
   // run the engine
   Engine.run(engine);
@@ -86,35 +256,10 @@
   // pass mouse to renderer to enable showMousePosition
   render.mouse = mouseConstraint.mouse;
 
-  let counter = 0;
-  const moveClimber = () => {
-    console.log(climber);
-
-    let leftSidePosition = 800;
-    let topSidePosition = 0;
-
-    for (let i = 0; i < climber.parts.length; i++) {
-      const climberPart = climber.parts[i];
-
-      leftSidePosition = (leftSidePosition > climberPart.position.x)
-        ? climberPart.position.x
-        : leftSidePosition;
-
-      topSidePosition = (topSidePosition > climberPart.position.y)
-        ? topSidePosition
-        : climberPart.position.y;
-    }
-
-    console.log(topSidePosition);
-    climber.positionImpulse.x += Math.abs(leftSidePosition - 300);
-    climber.positionImpulse.y -= Math.abs(topSidePosition - 500);
-  }
-
   Events.on(engine, 'beforeUpdate', (event) => {
-    counter++;
-
-    if (counter === 40) {
-      moveClimber();
-    }
+    const cogCoordinates = calculateCog([torso, hip, shoulderL, shoulderR, footL, footR,
+      elbowL, elbowR, handL, handR
+    ]);
+    Body.setPosition(cog, { x: cogCoordinates.x, y: cogCoordinates.y });
   });
 })();
